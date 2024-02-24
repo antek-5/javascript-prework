e7cf08543419094abc4e2a34c825e7b6677137e8
Antoni Jankowski <antek.jankowski.jv@gmail.com>
2024-02-24 17:42:37 +0100

    Add who is the winner to JS

--- js/script.js
+++ js/script.js
@@ -50,7 +50,45 @@ else if(playerInput == '3')
 
 printMessage('Twój ruch to: ' + playerMove);
 
+// WYNIK
 
-
+if( computerMove == 'kamień' && playerMove == 'kamień')
+{
+    printMessage('Remis!');
+}
+else if( computerMove == 'kamień' && playerMove == 'papier')
+{
+    printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce')
+{
+    printMessage('Przegrałeś! :(');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień')
+{
+    printMessage('Przegrałeś! :(');
+}
+else if( computerMove == 'papier' && playerMove == 'papier')
+{
+    printMessage('Remis!');
+}else if( computerMove == 'papier' && playerMove == 'nożyce')
+{
+    printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień')
+{
+    printMessage('Ty wygrywasz!');
+}else if( computerMove == 'nożyce' && playerMove == 'papier')
+{
+    printMessage('Przegrałeś! :(');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce')
+{
+    printMessage('Remis!');
+}
+else if( playerMove == 'nieznany ruch')
+{
+    printMessage('Nieznany Ruch :(');
+}
 
 
